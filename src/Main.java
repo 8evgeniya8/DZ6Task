7f69c19cbcdb85644869824fb8f9678813d854ec
Tsukanova Evgeniya <tata@gmail.com>
2023-09-18 15:29:01 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
-public class Main {
+import task1.PalindromeChecker;
 
+import static task1.PalindromeChecker.isPalindrome;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputString = "A man, a plan, a canal: Panama";
+        if (isPalindrome(inputString)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
 }
